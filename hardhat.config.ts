78c03726f0e1e0c5074338c57ce150c03de6566d
Greg Jeanmart <greg@jeanmart.me>
2024-05-14 20:57:36 +0200

    add deployment scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,22 @@
 import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ignition-ethers";
 import 'solidity-coverage'
 
 const SOLIDITY_VERSION = "0.8.24"
 const COINMARKETCAP_APIKEY = vars.get("COINMARKETCAP_APIKEY")
+const INFURA_API_KEY = vars.get("INFURA_API_KEY")
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY")
 const REPORT_GAS = vars.get("REPORT_GAS")
 
 const config: HardhatUserConfig = {
   solidity: SOLIDITY_VERSION,
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
   gasReporter: {
     enabled: Boolean(REPORT_GAS),
     currency: "USD",
